e643c7496071f680c747e20418aa9bccd99b0c07
Nicolas North <00770885@nz.is>
2019-02-03 17:59:48 +0100

    Also include combined profiles in output

--- streisand-profile-packager.sh
+++ streisand-profile-packager.sh
@@ -23,6 +23,7 @@ output_global_dir="${output_dir}/all"
 output_ov_dir="${output_global_dir}/ovpn"
 output_ov_sl_dir="${output_ov_dir}/sslh"
 output_ov_ud_dir="${output_ov_dir}/udp"
+output_ov_co_dir="${output_ov_dir}/combined"
 
 output_wg_dir="${output_global_dir}/wgrd"
 
@@ -30,13 +31,19 @@ output_users_dir="${output_dir}/users"
 
 ################################################################################
 
-if [ -d "./vpn-profiles" ]
+if [ -d "${output_dir}-old" ]
 then
-    mv "./vpn-profiles" "./vpn-profiles-old"
+    rm -r "${output_dir}-old"
+fi
+
+if [ -d "${output_dir}" ]
+then
+    mv "${output_dir}" "${output_dir}-old"
 fi
 
 mkdir -p "${output_ov_sl_dir}"
 mkdir -p "${output_ov_ud_dir}"
+mkdir -p "${output_ov_co_dir}"
 mkdir -p "${output_wg_dir}"
 
 ################################################################################
@@ -78,6 +85,24 @@ unset counter counter_name dir
 
 ################################################################################
 
+### openvpn combined
+
+counter=1
+
+for dir in "${streisand_ov_dir}"/*
+do
+    if test -d "${dir}"
+    then
+        counter_name="$(echo ${counter} | xargs printf %02d)"
+        cp ${dir}/*-combined.ovpn "${output_ov_co_dir}/${name_prefix}-ovpn-combined-${counter_name}.ovpn"
+        counter=$(($counter + 1))
+    fi
+done
+
+unset counter counter_name dir
+
+################################################################################
+
 ### wireguard
 
 counter=1
@@ -103,7 +128,7 @@ user_counter=1
 
 profile_counter=1
 
-while [ "${user_counter}" -le "5" ]
+while [ "${user_counter}" -le "20" ]
 do
 
     user_counter_name="$(echo ${user_counter} | xargs printf %02d)"
@@ -113,6 +138,7 @@ do
     user_output_ov_dir="${user_output_dir}/ovpn"
     user_output_ov_sl_dir="${user_output_ov_dir}/sslh"
     user_output_ov_ud_dir="${user_output_ov_dir}/udp"
+    user_output_ov_co_dir="${user_output_ov_dir}/combined"
 
     user_output_wg_dir="${user_output_dir}/wgrd"
 
@@ -120,19 +146,21 @@ do
 
     mkdir -p "${user_output_ov_sl_dir}"
     mkdir -p "${user_output_ov_ud_dir}"
+    mkdir -p "${user_output_ov_co_dir}"
     mkdir -p "${user_output_wg_dir}"
 
     ###
 
     user_profile_counter=1
 
-    while [ "${user_profile_counter}" -le "4" ]
+    while [ "${user_profile_counter}" -le "1" ]
     do
 
         profile_counter_name="$(echo ${profile_counter} | xargs printf %02d)"
 
         cp "${output_ov_sl_dir}/${name_prefix}-ovpn-sslh-${profile_counter_name}.ovpn" "${user_output_ov_sl_dir}"
         cp "${output_ov_ud_dir}/${name_prefix}-ovpn-udp-${profile_counter_name}.ovpn" "${user_output_ov_ud_dir}"
+        cp "${output_ov_co_dir}/${name_prefix}-ovpn-combined-${profile_counter_name}.ovpn" "${user_output_ov_co_dir}"
         cp "${output_wg_dir}/${name_prefix}-wrgd-${profile_counter_name}.conf" "${user_output_wg_dir}"
 
         profile_counter=$(($profile_counter + 1))
